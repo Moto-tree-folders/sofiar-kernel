3adf2020bf52a9badbfaf7f1b025f30a678b7f41
seba3567 <sebastian26481@gmail.com>
2021-02-14 20:48:48 -0300

    Revert "treewide: Linux 4.14.175"
    This reverts commit a83c58b6b35f18da84c0a98c5be8f838e3ed76ac.

--- arch/x86/kernel/ftrace.c
+++ arch/x86/kernel/ftrace.c
@@ -36,7 +36,6 @@
 #ifdef CONFIG_DYNAMIC_FTRACE
 
 int ftrace_arch_code_modify_prepare(void)
-    __acquires(&text_mutex)
 {
 	mutex_lock(&text_mutex);
 	set_kernel_text_rw();
@@ -45,7 +44,6 @@ int ftrace_arch_code_modify_prepare(void)
 }
 
 int ftrace_arch_code_modify_post_process(void)
-    __releases(&text_mutex)
 {
 	set_all_modules_text_ro();
 	set_kernel_text_ro();
